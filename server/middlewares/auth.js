f235cb2d2443e3417d37f1cc46d605c11afb639b
ranjhal23 <ranjhal2003@gmail.com>
2024-08-26 23:06:01 +0530

    auth done

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -1,3 +1,96 @@
 const jwt= require('jsonwebtoken')
 require('dotenv').config()
 const User= require('../models/User')
+
+//auth
+exports.auth= async(req, res, next) =>{
+    try{
+        //extract token
+        const token= req.cookies.token 
+                   || req.body.token
+                   || req.header('Authorisation').replace('Bearer', '')
+        if(!token){
+            return res.status(401).json({
+                success: false,
+                message: 'Token is missing'
+            })
+        }
+        //verify the token
+        try{
+            const decode=  jwt.verify(token, process.env.JWT_SECRET)
+            console.log(decode)
+            req.user= decode;
+
+        }catch(e){
+            return res.json(401).json({
+                success: false,
+                message: 'Token is invalid'
+            })
+        }
+        next()
+    } catch(e){
+        console.log(e)
+        return res.status(401).json({
+            success: false,
+            message: 'Something went wrong while validating token'
+        })
+
+    }
+}
+
+//isStudent
+exports.isStudent= async(req, res, next)=>{
+    try{
+        if(req.user.accountType!=='Student'){
+            return res.status(401).json({
+                success: false,
+                message:'This is protected route for students only'
+            })
+        }
+        next();
+
+    }catch(e){
+        return res.status(500).json({
+            success: false,
+            message: 'Issue in verifing the student'
+        })
+    }
+}
+
+//isInstructor
+exports.isInstructor= async(req, res, next)=>{
+    try{
+        if(req.user.accountType!=='Instructor'){
+            return res.status(401).json({
+                success: false,
+                message:'This is protected route for Instructor only'
+            })
+        }
+        next();
+
+    }catch(e){
+        return res.status(500).json({
+            success: false,
+            message: 'Issue in verifing the student'
+        })
+    }
+}
+
+//isAdmin
+exports.isAdmin= async(req, res, next)=>{
+    try{
+        if(req.user.accountType!=='Admin'){
+            return res.status(401).json({
+                success: false,
+                message:'This is protected route for Admin only'
+            })
+        }
+        next();
+
+    }catch(e){
+        return res.status(500).json({
+            success: false,
+            message: 'Issue in verifing the student'
+        })
+    }
+}

cce21de90dfe94b5827787da061e0bad9230142e
ranjhal23 <ranjhal2003@gmail.com>
2024-08-30 21:52:49 +0530

    new

---
+++ server/controllers/Tags.js
@@ -0,0 +1,45 @@
+const Tag= require('../models/tags')
+ 
+//create tag
+exports.createTag= async(req, res)=>{
+    try{
+        //fetch data
+        const {name, description}= req.body
+        //validate
+        if(!name || !description){
+            return res.status(400).json({
+                success: false,
+                message: 'All fields are required'
+            })
+        }
+        //create in db
+        const tagDetails= await Tag.create({
+            name: name,
+            description: description
+        })
+        console.log(tagDetails)
+        //return response
+        return res.status(200).json({
+            success: true,
+            message:'Tag created Successfully'
+        })
+
+    }catch(e){
+        return res.status(500).json({
+            success: false,
+            message: e.message
+        })
+    }
+}
+
+//getAllTags
+exports.showAlltags= async(req, res)=>{
+    try{
+
+    }catch(e){
+        return res.status(500).json({
+            success: false,
+            message: e.message
+        })
+    }
+}

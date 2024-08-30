6d0692b02a591d409ecc9603c3202a159c5f02f8
ranjhal23 <ranjhal2003@gmail.com>
2024-08-30 23:03:34 +0530

    changes

--- server/controllers/Tags.js
+++ server/controllers/Tags.js
@@ -35,7 +35,12 @@ exports.createTag= async(req, res)=>{
 //getAllTags
 exports.showAlltags= async(req, res)=>{
     try{
-
+        const allTags= await Tag.find({}, {name:true, description: true});
+        return res.status(200).json({
+            success: true,
+            message: 'All tags returned successfully',
+            allTags,
+        })
     }catch(e){
         return res.status(500).json({
             success: false,
